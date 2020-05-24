afc7fd74784b128f55c09ecaeb6d6c34a86a703c
senhaji <senhaji93@gmail.com>
2020-05-24 03:35:22 +0200

    add test triangle

--- src/test/java/senhaji/pglp_9_9/test/triangleTest.java
+++ src/test/java/senhaji/pglp_9_9/test/triangleTest.java
@@ -1,5 +1,29 @@
 package senhaji.pglp_9_9.test;
 
-public class triangleTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import senhaji.pglp_9_9.shape.Point;
+import senhaji.pglp_9_9.shape.Triangle;
+
+public class triangleTest {
+	@Test
+    public void testConstructor() {
+        Triangle t = new Triangle("triangle", new Point(3,3), new Point(5,1), new Point(5,3));
+        assertEquals(t.getNameshape(), "triangle");
+        assertTrue(t.getPoint(0).toString().equals("Point [ getX()=3, getY()=3]"));
+        assertTrue(t.getPoint(1).toString().equals("Point [ getX()=5, getY()=1]"));
+        assertTrue(t.getPoint(2).toString().equals("Point [ getX()=5, getY()=3]"));
+    }
+   
+    @Test
+    public void testMove() {
+    	  Triangle t = new Triangle("triangle", new Point(3,3), new Point(5,1), new Point(5,3));
+        t.movepoint(3, 3);
+        assertTrue(t.getPoint(0).toString().equals("Point [ getX()=6, getY()=6]"));
+        assertTrue(t.getPoint(1).toString().equals("Point [ getX()=8, getY()=4]"));
+        assertTrue(t.getPoint(2).toString().equals("Point [ getX()=8, getY()=6]"));
+    }
 }
