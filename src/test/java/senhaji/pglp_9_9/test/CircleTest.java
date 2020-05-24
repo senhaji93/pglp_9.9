0496c0794f2fc1411d5ba6704996caaa0c03857e
senhaji <senhaji93@gmail.com>
2020-05-24 03:47:44 +0200

    add test circle

--- src/test/java/senhaji/pglp_9_9/test/CircleTest.java
+++ src/test/java/senhaji/pglp_9_9/test/CircleTest.java
@@ -1,5 +1,27 @@
 package senhaji.pglp_9_9.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import senhaji.pglp_9_9.shape.Cercle;
+import senhaji.pglp_9_9.shape.Point;
+
 public class CircleTest {
 
+    @Test
+    public void testConstructor() throws Exception {
+        Cercle c= new Cercle("c", new Point(3,3), 5);
+        assertEquals(c.getNameshape(), "c");
+        assertTrue(c.getRayon() == 5);
+    }
+    
+    @Test
+    public void testmove() throws Exception {
+        Cercle c = new Cercle("c", new Point(3,3), 5);
+        c.movepoint(3, 3);
+        assertTrue(c.getCentre().toString().equals("Point [ getX()=6, getY()=6]"));
+    }
+    
 }
